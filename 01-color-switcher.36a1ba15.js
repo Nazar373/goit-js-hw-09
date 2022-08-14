8d238aa4af6e26f718ca87915e0f903f6b3c8197
Nazar373 <99971906+Nazar373@users.noreply.github.com>
2022-08-14 19:56:01 +0000

    Deploying to gh-pages from @ Nazar373/goit-js-hw-09@d976eaf66fff0bced05e16367d71c4a628bb9469 🚀

---
+++ 01-color-switcher.36a1ba15.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.startBtn.addEventListener("click",(()=>{e.start()})),t.stopBtn.addEventListener("click",(()=>{e.stop()}));const e={intervalId:null,start(){if(this.isActive)return;Date.now();this.isActive=!0,this.intervalId=setInterval((()=>{Date.now();document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)},stop(){clearInterval(this.intervalId),this.isActive=!1}};
+//# sourceMappingURL=01-color-switcher.36a1ba15.js.map
